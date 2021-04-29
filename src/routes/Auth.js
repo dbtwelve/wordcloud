b860f372eafc84fac23920a4ce0df8ad9f559647
Lee DongUk <>
2021-04-29 15:04:21 +0900

    Auth clean to make AuthForm

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,58 +1,27 @@
+import AuthForm from "components/AuthForm";
 import { authService, firebaseInstance } from "fbase";
-import React, { useState } from "react";
+import React from "react";
 
 //export default () => <span>Auth</span>;
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const onChange = (event) => {
-        const {target: {name, value}} = event;
-        if(name === "email"){
-            setEmail(value)
-        } else if(name === "password"){
-            setPassword(value)
+    
+    
+    const onSocialClick = async (event) => {
+        const {
+            target: {name},
+        }   =   event;
+        let provider;
+        if(name === "google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
         }
+        await authService.signInWithPopup(provider);
+
     };
-    const onSubmit = async(event) => {
-        event.preventDefault(); //이메일, 비밀번호 입력후 버튼누르면 화면 새로고침 방지
-        try{
-            let data;
-            if(newAccount){
-                data = await authService.createUserWithEmailAndPassword(email, password)
-            } else {
-                data = await authService.signInWithEmailAndPassword(email, password)
-            }
-            console.log(data)
-        } catch(error) {
-            setError(error.message);
-        }
-        
-    };
-const toggleAccount = () => setNewAccount((prev) => !prev) 
-const onSocialClick = async (event) => {
-    const {
-        target: {name},
-    }   =   event;
-    let provider;
-    if(name === "google"){
-        provider = new firebaseInstance.auth.GoogleAuthProvider();
-    } else if (name === "github"){
-        provider = new firebaseInstance.auth.GithubAuthProvider();
-    }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data)
-};
     return (
         <div >
-            <form onSubmit={onSubmit}>
-                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
-                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
-            </form>
-            {error}
-            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
+            <AuthForm/>
             <div>
                 <button onClick={onSocialClick} name="google">Continue with Google</button>
                 <button onClick={onSocialClick} name="github">Continue with Github</button>
